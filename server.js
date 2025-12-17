cc9a6c9dd09223cb467dd30cf7ff8bd0899f0080
Jithick <jithick12@gmail.com>
2025-12-17 11:44:52 +0530

    Country API ready for deployment process 4

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
 
   if (!rapidApiKey || !rapidApiHost) {
     return res.status(401).json({
-      error: "Unauthorized request. Must come via RapidAPI.",
+      error: "Unauthorized request. ",
     });
   }
   next();
